2e798fef1651f642ed420f76723d4f6c91a86d49
MrMustage <noam.rosenberg.private@gmail.com>
2022-09-08 17:04:59 +0200

    client to server now working!

---
+++ GreenLanChat/src/Main.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.net.*;
+import java.nio.channels.Channel;
+import java.nio.channels.Channels;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.util.Scanner;
+
+import Socket.*;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+
+        BClient bc  = BClient.start();
+        Scanner sc = new Scanner(System.in);
+
+        while (true){
+            bc.sendMessage(sc.next());
+        }
+    }
+}
---
+++ GreenLanChat/src/Socket/BClient.java
@@ -0,0 +1,57 @@
+package Socket;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.*;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+public class BClient {
+
+    private static SocketChannel client;
+    private static ByteBuffer buffer;
+    private static BClient instance;
+
+    public static BClient start() {
+        if (instance == null)
+            instance = new BClient();
+
+        return instance;
+    }
+
+    public static void stop() throws IOException {
+        client.close();
+        buffer = null;
+    }
+
+    private BClient() {
+        try {
+            client = SocketChannel.open(new InetSocketAddress("localhost", 5454));
+            buffer = ByteBuffer.allocate(256);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String sendMessage(String msg) {
+        buffer = ByteBuffer.wrap(msg.getBytes());
+        String response = null;
+        try {
+            client.write(buffer);
+            buffer.clear();
+            client.read(buffer);
+            response = new String(buffer.array()).trim();
+            System.out.println("response=" + response);
+            buffer.clear();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return response;
+
+    }
+}
