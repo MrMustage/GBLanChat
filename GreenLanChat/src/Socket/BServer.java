18715a33e5e82c886a31249d027837f3aea8b754
MrMustage <noam.rosenberg.private@gmail.com>
2022-09-18 13:30:36 +0300

    added scanKeyList() func to BServer and it returns the SelectionKey by the given ID

--- GreenLanChat/src/Socket/BServer.java
+++ GreenLanChat/src/Socket/BServer.java
@@ -2,9 +2,11 @@ package Socket;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.module.ResolutionException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.*;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -14,9 +16,11 @@ import java.util.Set;
 public class BServer {
 
     private static final String killString = "kill";
+    private static int id = 0;
     private static final int port = 5454;
-    private static String lastMSG=null;
-    private static List<SelectionKey> userList= new ArrayList<>();
+    private static String lastMSG = null;
+    private static List<SelectionKey> userList = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
         Selector selector = Selector.open();
         ServerSocketChannel serverSocket = ServerSocketChannel.open();
@@ -25,7 +29,7 @@ public class BServer {
         serverSocket.register(selector, SelectionKey.OP_ACCEPT);
         ByteBuffer buffer = ByteBuffer.allocate(256);
 
-        System.out.println("server started at port: " + port );
+        System.out.println("server started at port: " + port);
         while (true) {
 
             selector.select();
@@ -34,11 +38,13 @@ public class BServer {
 
             while (iter.hasNext()) {
                 SelectionKey key = iter.next();
-                    if (key.isAcceptable()) {
-                        register(selector, serverSocket);
-                        key.attach(1);
-                        userList.add(key);
-                    }
+                if (key.isAcceptable()) {
+                    register(selector, serverSocket);
+                    key.attach(id);
+                    id++;
+                    userList.add(key);
+
+                }
 
                 if (key.isReadable()) {
                     ReadKeyHandling(buffer, key);
@@ -46,26 +52,63 @@ public class BServer {
                 }
                 if (!key.isValid()) key.channel().close();
                 iter.remove();
-
+//                forwardMSG(buffer,scanKeysList(2),new Message(1,"work"));
             }
+//    public static SelectionKey scanKeysList(int userID) {
+//        for (SelectionKey sk : userList) {
+//            System.out.println();
+//            System.out.println("channel: " + sk.channel().toString() + "userID: " + sk.attachment());
+//
+//            if (sk.attachment().equals(userID)) {
+//                System.out.println(sk.channel().isOpen());
+//                System.out.println(sk.isWritable());
+//                System.out.println("found");
+//                return sk;
+//            }
+//        }
+//        return null;
+//    }
 
         }
     }
-    public static void ReadKeyHandling(ByteBuffer buffer,SelectionKey key) throws IOException {
+
+
+    public static void forwardMSG(ByteBuffer buffer, SelectionKey key, Message msg) throws IOException {
+        if(key == null){
+            return;
+        }
+        System.out.println("forwardMSG");
+
+        SocketChannel client = (SocketChannel) key.channel();
+
+        buffer.flip();
+        client.write(ByteBuffer.wrap((msg.getPayload() + " - " + msg.getDateTime().getBytes()).getBytes()));
+        //msg.setLocalTimeOBJ(LocalDateTime.now());
+        System.out.println(msg.getPayload() + " - " + msg.getDateTime());
+        buffer.clear();
+    }
+    public static void ReadKeyHandling(ByteBuffer buffer, SelectionKey key) throws IOException {
+        System.out.println("ReadKeyHandling");
         SocketChannel client = (SocketChannel) key.channel();
         client.read(buffer);
         String pure = new String(buffer.array()).trim();
-        //client.close();
-        if(pure.equals(lastMSG)){return;}
-        if(pure.equals("-1"))client.close();
+        if (pure.equals(lastMSG)) {
+            client.write(ByteBuffer.wrap("Kicked from server: spam".getBytes(StandardCharsets.UTF_8)));
+            System.out.println("Kicked from server: spam");
+            client.socket().close();
+            return;
+        }
+        if (pure.equals("-1")) client.close();
         buffer.flip();
         client.write(buffer);
         Message msg = new Message(new String(buffer.array()));
+
         //msg.setLocalTimeOBJ(LocalDateTime.now());
-        System.out.println(msg.getPayload()+" - "+msg.getDateTime());
+        System.out.println(msg.getPayload() + " - " + msg.getDateTime());
         buffer.clear();
-        lastMSG=pure;
+        lastMSG = pure;
     }
+
     private static void answerWithEcho(ByteBuffer buffer, SelectionKey key) throws IOException {
 
         SocketChannel client = (SocketChannel) key.channel();
@@ -73,23 +116,25 @@ public class BServer {
         if (new String(buffer.array()).trim().equals(killString)) {
             client.close();
             System.out.println("Not accepting client messages anymore");
-        }
-        else {
+        } else {
             buffer.flip();
             client.write(buffer);
             Message msg = new Message(new String(buffer.array()));
             msg.setLocalTimeOBJ(LocalDateTime.now());
-            System.out.println(msg.getPayload()+" - "+msg.getDateTime());
+            System.out.println(msg.getPayload() + " - " + msg.getDateTime());
             buffer.clear();
         }
     }
+
     private static void register(Selector selector, ServerSocketChannel serverSocket)
             throws IOException {
+        System.out.println("register");
         SocketChannel client = serverSocket.accept();
         client.configureBlocking(false);
 
         client.register(selector, SelectionKey.OP_READ);
     }
+
     //public static SelectionKey findByID(int i){return };
     public static Process start() throws IOException {
         String javaHome = System.getProperty("java.home");
@@ -99,5 +144,24 @@ public class BServer {
         ProcessBuilder builder = new ProcessBuilder(javaBin, "-cp", classpath, className);
         return builder.start();
     }
+
+    public static void sendMSG(Message msg) {
+
+    }
+//
+//    public static SelectionKey scanKeysList(int userID) {
+//        for (SelectionKey sk : userList) {
+//            System.out.println();
+//            System.out.println("channel: " + sk.channel().toString() + "userID: " + sk.attachment());
+//
+//            if (sk.attachment().equals(userID)) {
+//                System.out.println(sk.channel().isOpen());
+//                System.out.println(sk.isWritable());
+//                System.out.println("found");
+//                return sk;
+//            }
+//        }
+//        return null;
+//    }
 
 }
