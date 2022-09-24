83347c4d0a3c3d527784c2be594120093fb7e132
MrMustage <noam.rosenberg.private@gmail.com>
2022-09-24 20:38:23 +0300

    added toID int to message

--- GreenLanChat/src/Socket/Message.java
+++ GreenLanChat/src/Socket/Message.java
@@ -6,27 +6,28 @@ public class Message {
     private int type;
     private String payload;
     LocalDateTime DateTime;
+
+    private int toID;
     final private DateTimeFormatter DateFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
     public Message(Message message){
         this.type=message.getType();
         this.payload=message.getPayload();
         this.DateTime=message.getDateTimeOBJ();
+        this.toID = message.getToID();
     }
     public Message(int type,String payload){
         this.type=type;
         this.payload=payload;
         this.DateTime=LocalDateTime.now();
     }
-    public Message(int type,String payload,LocalDateTime DateTime){
+    public Message(int type,String payload,LocalDateTime DateTime, int toID){
         this.type=type;
         this.payload=payload;
         this.DateTime=DateTime;
+        this.toID = toID;
     }
-    public Message(int type,String payload,String DateTime){
-        this.type=type;
-        this.payload=payload;
-        this.DateTime=LocalDateTime.parse(DateTime,DateFormat);
-    }
+
+
     public void setType(int type){this.type = type;}
     public int getType(){return this.type;}
     public void setPayload(String payload){this.payload = payload;}
@@ -36,12 +37,20 @@ public class Message {
     public void setDateTimeOBJ(LocalDateTime DateTime){this.DateTime=DateTime;}
     public LocalDateTime getDateTimeOBJ(){return this.DateTime;}
     public void setLocalTimeOBJ(LocalDateTime DateTime){this.DateTime=DateTime;}
+
+    public int getToID (){
+        return this.toID;
+    }
+    public void setToID(int i){
+        this.toID = i;
+    }
     @Override
-    public String toString(){return this.type+","+this.payload+","+this.DateTime.format(DateFormat)+",";}
+    public String toString(){return this.type+","+this.payload+","+this.DateTime.format(DateFormat)+","+toID;}
     public Message(String message){
         String[] info = message.split(",");
         this.type=Integer.valueOf(info[0]);
         this.payload=info[1];
         this.DateTime=LocalDateTime.parse(info[2],DateFormat);
+        this.toID = Integer.valueOf(info[3]);
     }
 }
